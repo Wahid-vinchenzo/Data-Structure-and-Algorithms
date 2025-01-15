56ad14a36562d92c3a4d8320fc285d6c191765b4
Wahid-vinchenzo <ravinshubo@gmail.com>
2025-01-15 20:53:46 +0600

    selection sort

---
+++ selectionsort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {100, 20, 10, 2, 30, 40, 15, 35, 25, 55, 60};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
